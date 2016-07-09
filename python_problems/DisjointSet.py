3613eb6bc57ce4b00c588e0fe664165a33221a72
lorenzocastillo <lcast042@fiu.edu>
2016-07-09 19:47:26 -0400

    Added Documentation

--- python_problems/DisjointSet.py
+++ python_problems/DisjointSet.py
@@ -1,28 +1,45 @@
 class DisjointSet:
-    def __init__(self, elements):
-        self.arr = []
-        self.size = [1]*elements
-        self.count = elements
-        for i in range(elements):
-            self.append(i)
+    def __init__(self, n):
+        self.arr = list()
+        self.size = [1] * n
+        self.count = n
 
-    def find(self,a,b):
+        for i in range(n):
+            self.arr.append(i)
+
+    def find(self, a, b):
+        """
+        if a and b have the same root, then they're in the same connected component
+        :param a:
+        :param b:
+        :return:
+        """
         return self.root(a) == self.root(b)
 
-    def union(self,a,b):
+    def union(self, a, b):
+        """
+        Get the root of a and b, and attach the smaller tree to the bigger tree
+        :param a:
+        :param b:
+        :return:
+        """
         root_a = self.root(a)
         root_b = self.root(b)
         if root_a == root_b:
             return
-        if self.size[root_a] < self.size[root_b]:
-            self.arr[root_a] = root_b
-            self.size[root_b] += self.size[root_a]
-        else:
-            self.arr[root_b] = root_a
-            self.size[root_a] += self.size[root_b]
+
+        root_a, root_b = (root_a, root_b) if self.size[root_a] < self.size[root_b] else (root_b, root_a)
+
+        self.arr[root_a] = root_b
+        self.size[root_b] += self.size[root_a]
         self.count -= 1
 
     def root(self, a):
+        """
+        Finds the root of a. Then proceeds to make all nodes in that path to have the same root as a for path compression
+        :param a:
+        :return:
+        """
         cur = self.arr[a]
         while cur != self.arr[cur]:
             cur = self.arr[cur]
@@ -35,8 +52,9 @@ class DisjointSet:
         return root_a
 
     def __repr__(self):
-        return self.arr.__repr__()
+        return repr(self.arr)
 
 
 ds = DisjointSet(10)
 ds.union(0,1)
+print(ds)
