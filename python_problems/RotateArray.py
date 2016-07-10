c6e1ed9afb50e5ee04034436059f2755c22ed3a3
lorenzocastillo <lcast042@fiu.edu>
2016-07-10 19:33:51 -0400

    Added Test Cases

--- python_problems/RotateArray.py
+++ python_problems/RotateArray.py
@@ -5,14 +5,12 @@ ex:
 [1, 2, 3, 4], k = -1 => [2, 3, 4, 1]
 [1, 2, 3, 4], k = 2 => [3, 4, 1, 2]
 """
-
-def rotate(arr, k=1):
-    return arr[k:] + arr[:k]
+from helpers.TestSuite import Assert
 
 
 def rotate_in_place(arr, k=1):
     """
-    First, reverse the entire array. Then reverse the array from [0, k) and again from [k, n)
+    First, reverse the entire array. Then, reverse the array from [0, k) and again from [k, n)
     :param arr: array to be rotated
     :param k: number of times for array to be rotated
     :return:
@@ -25,9 +23,20 @@ def rotate_in_place(arr, k=1):
     n = len(arr)
     # converts negative to positive and if k > n.
     k = (k + n) % n
+    arr = arr.copy()
     reverse(0, n - 1)
     reverse(0, k - 1)
     reverse(k, n - 1)
     return arr
 
-print(rotate_in_place([1,2,3,4,5,6,7], -2))
+
+def test():
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    f = rotate_in_place
+    Assert([3, 4, 5, 6, 7, 1, 2], f, arr, -2)
+    Assert([1, 2, 3, 4, 5, 6, 7], f, arr, 0)
+    Assert([6, 7, 1, 2, 3, 4, 5], f, arr, 2)
+    Assert([6, 7, 1, 2, 3, 4, 5], f, arr, 9)
+
+if __name__ == '__main__':
+    test()
