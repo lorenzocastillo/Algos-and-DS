5a8d2bf8a652db59ad9d46dfd22a2b8e56821447
lorenzocastillo <lcast042@fiu.edu>
2016-07-10 14:43:49 -0400

    Added Documentation Test

--- python_problems/PowerSets.py
+++ python_problems/PowerSets.py
@@ -1,5 +1,12 @@
+"""
+Generate all the subsets of a set
+"""
+from helpers.TestSuite import Assert
+
+
 def distribute(value, values):
-    return list(map(lambda subset: [value] + subset, values))
+    return [[value] + subset for subset in values]
+    # return list(map(lambda subset: [value] + subset, values)) list comprehensions is ~ 25-50% faster
 
 
 def powersets(set_,i=0):
@@ -10,30 +17,34 @@ def powersets(set_,i=0):
 
 
 def gen_binaries(up_to, n):
-    bins = []
+    bins = list()
     for i in range(0, up_to):
         binary_i = bin(i)[2:]
         binary_i = '0' * (n - len(binary_i)) + binary_i
         bins.append(binary_i)
     return bins
 
+
 def powersets_alt(set_):
     n = len(set_)
     total_sets = 2 ** n
     binaries = gen_binaries(total_sets,n)
-    result = []
+    result = list()
     for binary in binaries:
         res = [set_[i] for i,digit in enumerate(binary) if digit == '1']
-        for i,digit in enumerate(binary):
-            if digit == '1':
-                res.append(set_[i])
         result.append(res)
     return result
 
-print(powersets([]))
-print(powersets([1]))
-print(powersets([2,3]))
-print(powersets([1,2,3]))
+
+def test():
+    arr = []
+    f = powersets
+    Assert([[]], f, arr)
+    arr = [1, 2, 3, 4]
+    solution = [[1, 2, 3, 4], [1, 2, 3], [1, 2, 4], [1, 2], [1, 3, 4], [1, 3], [1, 4], [1], [2, 3, 4], [2, 3], [2, 4],
+                [2], [3, 4], [3], [4], []]
+    Assert(solution, f, arr)
 
 
-print(powersets_alt([1, 2, 3]))
+if __name__ == '__main__':
+    test()
