0b917ab3d45f86d46f138b47a00e7ae8c26d8674
lorenzocastillo <lcast042@fiu.edu>
2016-07-10 14:03:54 -0400

    Added Documentation Test Cases

--- python_problems/MajorityElement.py
+++ python_problems/MajorityElement.py
@@ -1,35 +1,29 @@
+"""
+Given a list of items, determine if there's a majority element. A majority element is one in which its count is greater
+than floor(len(lst)/2).
+"""
+from helpers.TestSuite import Assert
+
+
 def validate_majority(arr, possible_majority):
     count = 0
     for elem in arr:
         if elem == possible_majority:
             count += 1
 
-    return possible_majority if count > int(len(arr)/2) else -1
+    return possible_majority if count > len(arr)//2 else None
 
 
 def majority_element(arr):
-    def m_e(left, right):
-        if left > right:
-            return -1
-        elif right == left:
-            return arr[left]
-        else:
-            mid = int((left + right)/2)
-            majority_left = m_e(left, mid)
-            majority_right = m_e(mid + 1, right)
-            if majority_left == majority_right or (min(majority_left,majority_right) == -1 and max(majority_left, majority_right) != -1):
-                return max(majority_left, majority_right)
-            else:
-                return -1
-    possible_majority = m_e(0, len(arr)-1)
-    if possible_majority == -1:
-        return -1
-    return validate_majority(arr, possible_majority)
-
-
-def majority_element_better(arr):
+    """
+    Walk through the array, keeping track of the potential majority element. Increment the count if you encounter that
+    element again, or decrement the count if you encounter another element. If the count reaches 0, reset the majority
+    element with the next element you see. Lastly, verify that the element is the majority element
+    :param arr:
+    :return:
+    """
     count = 0
-    possible_majority = 0
+    possible_majority = arr[0]
     for elem in arr:
         if count == 0:
             possible_majority = elem
@@ -39,3 +33,16 @@ def majority_element_better(arr):
             count -= 1
 
     return validate_majority(arr, possible_majority)
+
+
+def test():
+    arr = [1, 2, 3, 4, 5]
+    f = majority_element
+    Assert(None, f, arr)
+    arr = [1, 1, 1, 2, 4]
+    Assert(1, f, arr)
+    arr = [1, 1, 2, 4, 4]
+    Assert(None, f, arr)
+
+if __name__ == '__main__':
+    test()
