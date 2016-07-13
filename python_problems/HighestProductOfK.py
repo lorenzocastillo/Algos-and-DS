30ab825740f3f86a7a25c29f62fcb6b2b77da13f
lorenzocastillo <lcast042@fiu.edu>
2016-07-13 16:47:42 -0400

    Added memoization

--- python_problems/HighestProductOfK.py
+++ python_problems/HighestProductOfK.py
@@ -1,53 +1,38 @@
-from functools import reduce
-from TestSuite import Assert
-
 """
 Given a list_of_ints, find the highest_product you can get from k of the integers.
 """
-def highest_product_of_3(arr, k=3):
-    if len(arr) < k:
-        return -1
-    if k == 1:
-        return max(arr)
-    elif k == 2:
-        sorted_arr = sorted(arr)
-        return max(sorted_arr[0]*sorted_arr[1], sorted_arr[-1]*sorted_arr[-2])
+from helpers.TestSuite import Assert
+from helpers.TraceCalls import TraceCalls
 
 
-    lowest_k_minus_2 = sorted(arr[:k-1])[:k-2]
-    highest_k_minus_2 = sorted(arr[:k-1], reverse=True)[:k-2]
-    lowest_prod_of_k_minus_one = 1
-    highest_prod_of_k_minus_one = 1
+def memoize(f):
+    memo = dict()
 
-    for cur in arr[:k-1]:
-        highest_prod_of_k_minus_one *= cur
-        lowest_prod_of_k_minus_one *= cur
+    def helper(*x):
+        if x not in memo:
+            memo[x] = f(*x)
+        return memo[x]
+    return helper
 
-    highest_prod_of_k = highest_prod_of_k_minus_one * arr[k-1]
 
-    arr = sorted(arr)
-
-    for i in range(0, len(arr)):
-        cur = arr[i]
-
-        highest_prod_of_k = max(highest_prod_of_k, lowest_prod_of_k_minus_one * cur, highest_prod_of_k_minus_one * cur)
-
-        potential_lowest = reduce(lambda x, y: x * y, lowest_k_minus_2, cur)
-        potential_highest = reduce(lambda x,y: x * y, highest_k_minus_2, cur)
-
-        lowest_prod_of_k_minus_one = min(lowest_prod_of_k_minus_one, potential_highest, potential_lowest)
-        highest_prod_of_k_minus_one = max(highest_prod_of_k_minus_one, potential_highest, potential_lowest)
-
-        lowest_k_minus_2 = sorted(lowest_k_minus_2 + [cur])[:k - 2]
-        highest_k_minus_2 = sorted(highest_k_minus_2 + [cur], reverse=True)[:k - 2]
-
-    return highest_prod_of_k
+def highest_prod_k(arr, k):
+    @memoize
+    def f(i, k):
+        if i == len(arr):
+            return 1, 1
+        elif k == 0:
+            return 1, 1
+        else:
+            res1, res2 = f(i + 1, k - 1)
+            res3, res4 = f(i+1, k)
+            return max(arr[i] * res1, arr[i]*res2, res3, res4), \
+                min(arr[i] * res1, arr[i]*res2, res3, res4)
+    return max(f(0, k))
 
 
 def test():
-    f = highest_product_of_3
-    arr = [2,1,4,6,5,5,3]
-
+    f = highest_prod_k
+    arr = [2, 1, 4, 6, 5, 5, 3]
     Assert(150, f, arr, 3)
     Assert(600,f, arr, 4)
     arr = [-1,-100,3,4,2,4,5]
@@ -56,6 +41,16 @@ def test():
     arr = [1, 2, 3, -10, 1, -10, 1, -10, 2, -10]
     Assert(30000, f, arr, 5)
     Assert(300, f, arr, 3)
+    arr = [-100, -100, 4, 2, 3, 8]
+    Assert(80000, f, arr, 3)
 
 if __name__ == '__main__':
     test()
+
+from timeit import Timer
+setup = """
+from __main__ import highest_prod_k
+arr = [1, 2, 3, -10, 1, -10, 1, -10, 2, -10, 4, 3, 5, 1, 2, 6, 10, -2, 10, 9, -9, 1, 2, 3, 4, 5]
+"""
+t = Timer(setup=setup, stmt="highest_prod_k(arr, 5)").timeit(100)
+print(t)
